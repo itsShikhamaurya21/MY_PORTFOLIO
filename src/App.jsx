72361bf1847e31519f4fe0ddf1097a60ec67b1c5
Shikha <shikhamaurya987373@gmail.com>
2025-08-15 22:53:38 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,4 @@
 import Home from "./Components/Home";
-import Navbar from "./Components/Navbar";
-import AboutMe from "./Components/AboutMe";
-import Education from "./Components/Education";
-import Experience from "./Components/Experience";
-import Skills from "./Components/Skills";
-import DSAJourney from "./Components/DSAJourney";
-import Certifications from "./Components/Cerifications";
-import Contactme from "./Components/Contactme";
-import Projects from "./Components/Projects";
-import Footer from "./Components/Footer";
 
 function App() {
   return (
